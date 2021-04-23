2485f476f85461a682eeb631f0e84bbdbce85e81
Fernando <fernandomirandalolzeiro@gmail.com>
2021-04-23 14:27:19 -0300

    first commit

---
+++ BruteForce.py
@@ -0,0 +1,43 @@
+import itertools
+import json
+
+
+def ReadJson():
+    with open('Graph_districts.json', 'r', encoding='utf8') as f:
+        return json.load(f)
+
+
+def HamCycle(graph):
+    routes = itertools.permutations(graph)
+    hamilt_route = []
+    quant_vert = graph.__len__()
+    
+    for route in routes:
+        last = -1
+        path = []
+        for district in route:
+            
+            if(last == -1):
+                last = district
+                path.append(district)
+                continue
+
+            for key_district in graph:
+                if(graph[district][key_district] == 1 and graph[key_district] in graph[last]):
+                    print("entrou")
+                    last = district
+                    path.append(district)
+                    break 
+
+            if(len(path) == quant_vert):
+                if(graph[last][path[0]] == 1):
+                    path.append(path[0])
+                    hamilt_route.append(path)
+    return hamilt_route
+
+
+if __name__ == '__main__':
+   graph = ReadJson()
+   district_routes = HamCycle(graph)
+   for district in district_routes:
+       print(district)
